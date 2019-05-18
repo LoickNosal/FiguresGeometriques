07234e84a2547e58050f1b034fe9f770015ba754
Nosal Loïck <loick.nosal5@etu.univ-lorraine.fr>
2019-05-18 17:46:13 +0200

    finit methode modifierPoint Carre

--- src/modele/Carre.java
+++ src/modele/Carre.java
@@ -21,42 +21,33 @@ public class Carre extends Rectangle{
 		ArrayList<Point> rec = new ArrayList<Point>(2);
 		
 		if (pts.size()>1){
-			
-			rec.add(pts.get(0));
-			
 			int x = 0;
 			int y = 0;
+			int diff = Math.abs(pts.get(0).rendreX() - pts.get(1).rendreX());
 			
-			int DiffX = pts.get(1).rendreX() - pts.get(0).rendreY();
-			int DiffY = pts.get(1).rendreY() - pts.get(0).rendreY();
-			
-			if (DiffX > 0 && DiffY > 0) { //coin bas droite
-				if (DiffX > DiffY) {
-					x = pts.get(0).rendreX() + DiffY;
-					y = pts.get(0).rendreY() + DiffY;
-				}else {
-					x = pts.get(0).rendreX() + DiffX;
-					y = pts.get(0).rendreY() + DiffX;
-				}
-				
-			}else if(DiffX <0 && DiffY > 0){ //coin bas gauche
-				x = pts.get(0).rendreX() - DiffX;
-				y = pts.get(0).rendreY() + DiffX;
-			}else if(DiffX >= 0 && DiffY <= 0) { //coin haut droit
-				x = pts.get(0).rendreX() + DiffX;
-				y = pts.get(0).rendreY() - DiffX;
-			}else if(DiffX <= 0 && DiffY <= 0) { //coin haut gauche
-				x = pts.get(0).rendreX() - DiffY;
-				y = pts.get(0).rendreY() - DiffY;
-			}else if(DiffX == DiffY) {
-				x = pts.get(1).rendreX();
-				y = pts.get(1).rendreY();
+			//coin haut gauche bas droite
+			if (pts.get(0).rendreY() < pts.get(1).rendreY() && pts.get(0).rendreX() < pts.get(1).rendreX()){
+				x = pts.get(0).rendreX()+diff;
+				y = pts.get(0).rendreY()+diff;
+			}
+			//coin bas droite haut gauche
+			else if (pts.get(0).rendreY() > pts.get(1).rendreY() && pts.get(0).rendreX() > pts.get(1).rendreX()){
+				x = pts.get(0).rendreX()-diff;
+				y = pts.get(0).rendreY()-diff;
+			}
+			//coin haut droite bas gauche
+			else if ( pts.get(0).rendreY() < pts.get(1).rendreY() && pts.get(0).rendreX() > pts.get(1).rendreX()){
+				x = pts.get(0).rendreX()-diff;
+				y = pts.get(0).rendreY()+diff;
+			}
+			//coin bas gauche haut droite
+			else {
+				x = pts.get(0).rendreX()+diff;
+				y = pts.get(0).rendreY()-diff;
 			}
 			
+			rec.add(pts.get(0));
 			rec.add(new Point(x,y));
-			
-
-			
 		}
 		super.modifierPoints(rec);
 	}
